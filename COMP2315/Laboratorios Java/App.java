a66c94ccc5ed12f0e7b5b9cacf1dd94853ad2631
wcolon-LM <colonwesley03@gmail.com>
2024-03-06 08:33:11 -0400

    update

---
+++ COMP2315/Laboratorios Java/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println(x:"Hello, World Bienvenidos!");
+        System.out.println(x:"bienvenidos al curso de COMP2315!");
+    }
+}
